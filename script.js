81e82ab65346a84533572361960a6d2c2d59b3b9
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-03-25 20:36:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function decimalToBinary(num) {
   // your code here
+	if(num===0)  return "0";
+	let sum = "";
+	while(num>0){
+	let rem=num%2;
+	sum=rem+sum;
+	num=Math.floor(num/2);
+	}
+	return sum;
 }
 const num = parseInt(prompt("Enter a number."));
 alert(decimalToBinary(num));
